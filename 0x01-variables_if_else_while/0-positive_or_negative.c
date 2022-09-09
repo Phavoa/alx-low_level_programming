4e6a6ce1910f18b24220f555f7f6c4adf7f5dec9
root <efemiayafavour@gmail.com>
2022-09-09 14:58:11 -0700

    initial push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,11 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * 
+ * Return: always sucess
+ */
 int main(void)
 {
 int n;
@@ -10,6 +15,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
+
 if (n > 0)
 { printf("%d is positive\n", n);
 }
@@ -17,7 +23,7 @@ else if (n == 0)
 {printf("%d is zero\n", n);
 }
 else
-{printf("%d is negetive\n", n);
+{printf("%d is negative\n", n);
 }
 return (0);
 }
