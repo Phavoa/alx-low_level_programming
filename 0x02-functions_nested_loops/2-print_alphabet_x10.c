169cb0bc1bf52d91552fc2e20c07ff76d4db4df5
root <efemiayafavour@gmail.com>
2022-09-14 14:20:12 -0700

    repeat a to z 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 times - print a to z 10 times
+ * Return nothing: 0
+ */
+
+void print_alphabet_x10(void);
+{
+int a;
+char c;
+for (a = 0; a < 10; a++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar("\n");
+}
+}
