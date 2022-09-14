b6657514d4b8275b1ffd6da0685651606a859b11
root <efemiayafavour@gmail.com>
2022-09-14 14:37:03 -0700

    repeat a to z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 times - repest the print_alphabet 10 times
+ * print_alphabet_x10 times - repeats the print_alphabet 10 times.
  * Return Nothing.
  */
 
