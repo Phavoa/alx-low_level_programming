7a128a3c01ab141e7bf7c89c001e8105493375a4
root <efemiayafavour@gmail.com>
2022-09-14 12:59:13 -0700

    a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints the string
+ * Return: 0 on success.
+ */
+void print_alphabet(void);
+{
+char alpha;
+
+for(alpha = 'a'; alpha <= 'z';alpha++);
+{
+_putchar('alpha');
+}
+{
+_putchar('\n');
+}
+return (0);
+}
+	
