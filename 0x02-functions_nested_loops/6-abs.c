718668da7a57e656f9346e11e8bdd45c4e2e4020
root <efemiayafavour@gmail.com>
2022-09-14 18:51:29 -0700

    compute ad so lut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_abs - calculate the absolute value of an integer.
+ * @i: the integer to be converted.
+ * Return: 0
+ */
+int _abs(int)
+{
+if (i > 0 || == 0)
+return (i);
+else
+return (i * -1);
+}
