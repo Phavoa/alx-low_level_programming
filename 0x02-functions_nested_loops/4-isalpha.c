cffce07d4ab0026f823b20cd285bf700c038e678
root <efemiayafavour@gmail.com>
2022-09-14 17:23:56 -0700

    program to check for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - test if a character is an alphabet or not.
+ * @c: character of alphabet.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
+ */
+int _isalpha(int c);
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
