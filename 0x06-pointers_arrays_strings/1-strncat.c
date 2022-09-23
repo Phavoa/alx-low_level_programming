9f1b358503a08347142bbb2bfecb582b6761ce46
root <efemiayafavour@gmail.com>
2022-09-22 21:21:47 -0700

    done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _strncat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: input of max bytes to be used
+ * Return: Always 0 (Success)
  */
-int main(void)
+char *_strncat(char *dest, char *src, int n)
 {
-char s1[98] = "Hello ";
-char s2[] = "World!\n";
-char *p;
+int i, co;
 
-	printf("%s\n", s1);
-	printf("%s", s2);
-	p = _strncat(s1, s2, 1);
-	printf("%s\n", s1);
-	printf("%s", s2);
-	printf("%s\n", p);
-	p = _strncat(s1, s2, 1024);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", p);
-return (0);
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+
+for (co = 0; co < n; co++)
+{
+dest[i + co] = src[co];
+if (src[co] == '\0')
+co = n;
+}
+
+return (dest);
 }
