cf0a8a825fb7ab5529b3fbc42d900ac6068bc939
root <efemiayafavour@gmail.com>
2022-09-30 11:45:48 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print the number of an argument
+ * @argc: number of command line arguments
+ * @argv: prints name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
