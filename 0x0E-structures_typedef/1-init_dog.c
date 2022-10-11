7ee45b8d471f128553eaac7085bd02730efd401a
root <efemiayafavour@gmail.com>
2022-10-11 09:56:29 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,13 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(void)name;
-	(void)age;
-	(void)owner;
 if (d != NULL)
 	{
-	(*d).name = "lezzy";
-	(*d).age = 24.5;
-	(*d).owner = "Efemiaya";
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 	}
 }
