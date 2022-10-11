91822a1e70fc67f21298a603f23a090fb08d09a5
root <efemiayafavour@gmail.com>
2022-10-10 17:26:28 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,12 @@ int main(int argc, char *argv[])
 	int i;
 	int multiply = 1;
 
-	if (i >= 1);
-	
-	for(i = 1; i < argc; i++)
+	if (i >= 1)
+	{
+	for (i = 1; i < argc; i++)
 	{
 	multiply = multiply * atoi(argv[i]);
+	}
 	}
 	printf("%d\n", multiply);
 	return (0);
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
