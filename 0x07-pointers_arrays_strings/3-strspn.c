6286ad4b2cb50f9d534c882019d097d54c47a132
root <efemiayafavour@gmail.com>
2022-11-21 09:40:35 -0800

    done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - gets the length of a preix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ * Return:  the number of bytes in the initial segment of
+ * s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, k, l, m = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	for (j = 0; accept[j] != '\0'; j++)
+	{}
+	for (k = 0; k < i; k++)
+	{
+	for (l = 0; l < j; l++)
+	{
+	if (accept[j] != s[i])
+	break;
+	else if (accept[k] == s[i])
+	{
+	m++;
+	break;
+	}
+	}
+	}
+	return (m);
+}
