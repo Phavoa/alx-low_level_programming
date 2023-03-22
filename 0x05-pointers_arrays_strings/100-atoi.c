ce66f761e827b577259a4c6200ded4be837ece96
root <efemiayafavour@gmail.com>
2023-03-22 08:13:38 -0700

    corrected 100_atoi.c and 101-keygen.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,10 +3,24 @@
 /**
  * _atoi - Entry point
  * @s: input
- * Return: Always 0 (Success)
+ * Return: Always the interger value of the converted string.
  */
 int _atoi(char *s)
 {
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-return (*s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+
+	}
+return (sign * result);
 }
+
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Entry point
- *
+ * main - generates random passwords for 101-crackme
  * Return: Always 0 (Success)
- */
+**/
+
 int main(void)
 {
+	int sum;
+	char c;
 
-return (0);
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
 }
