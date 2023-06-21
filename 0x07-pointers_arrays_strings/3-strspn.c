8a16748cbb1d811a91168223ec926f4882c0f1a0
root <efemiayafavour@gmail.com>
2023-06-21 12:43:42 -0700

    corrected 3-strspn,c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,27 +9,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, k, l;
-	unsigned int m = 0;
-
-	for (i = 0; s[i] != ','; i++)
-	{}
-	for (j = 0; accept[j] != '\0'; j++)
-	{}
-	for (k = 0; k < i; k++)
-	{
-	for (l = 0; l < j; l++)
-	{
-	if (accept[l] == s[k])
-	{
-	m++;
-	break;
-	}
-	else if (accept[j + 1] == '\0')
-	{
-	return (m);
-	}
-	}
-	}
-	return (m);
+    int i, j, num = 0;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                num++;
+                break;
+            }
+        }
+        if (accept[j] == '\0')
+        {
+            break;
+        }
+    }
+    return num;
 }
