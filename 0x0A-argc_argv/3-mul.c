26cd2b8770cddb4cefa06cd226d6cebeff44ff99
efemiaya favour <efemiayafavour@gmail.com>
2023-08-02 01:54:49 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,18 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int multiply = 1;
-	
-	if (argc == 1 || argc == 2)
+	int multiply;
+
+	if (argc != 3)
+	{
 		printf("Error\n");
-	else if(argc > 1)
-	{
-	for (i = 1; i < 3; i++)
-	{
-	multiply = multiply * atoi(argv[i]);
+		return (1);
 	}
+	multiply = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", multiply);
-	}
-	return (0);
+	return 0;
 }
